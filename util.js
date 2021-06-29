1cf77dbfb4715c09211578b743e5174edc0722ad
kz-fe <741180788@qq.com>
2021-06-29 20:07:39 +0800

    update: parse url params

--- util.js
+++ util.js
@@ -208,25 +208,27 @@ export function uniqueObjectArray(arg) {
 
 // get url params
 export function getUrlParams(url) {
-  var theRequest = new Object();
-  if (url.indexOf("?") != -1) {
-    var str = url.substr(1);
-    var strs = str.split("&");
-    for (var i = 0; i < strs.length; i++) {
-      theRequest[strs[i].split("=")[0]] = (strs[i].split("=")[1]);
+  const paramsStr = /.+\?(.+)$/.exec(url)[1]; // 将 ? 后面的字符串取出来
+  const paramsArr = paramsStr.split('&'); // 将字符串以 & 分割后存到数组中
+  let paramsObj = {};
+  // 将 params 存到对象中
+  paramsArr.forEach(param => {
+    if (/=/.test(param)) { // 处理有 value 的参数
+      let [key, val] = param.split('='); // 分割 key 和 value
+      val = decodeURIComponent(val); // 解码
+      val = /^\d+$/.test(val) ? parseFloat(val) : val; // 判断是否转为数字
+
+      if (paramsObj.hasOwnProperty(key)) { // 如果对象有 key，则添加一个值
+        paramsObj[key] = [].concat(paramsObj[key], val);
+      } else { // 如果对象没有这个 key，创建 key 并设置值
+        paramsObj[key] = val;
+      }
+    } else { // 处理没有 value 的参数
+      paramsObj[param] = true;
     }
-  }
-  return theRequest;
-}
+  })
 
-
-export function parseUrlSearch() {
-  const search = '?age=25&name=TYJ' // { age: "25", name: "TYJ" }
-  return search.replace(/(^\?)|(&$)/g, "").split("&").reduce((t, v) => {
-    const [key, val] = v.split("=");
-    t[key] = decodeURIComponent(val);
-    return t;
-  }, {});
+  return paramsObj;
 }
 
 
